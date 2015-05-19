5d43b8db6a58cb10f2cbfa11f682377832cd7765
mario <mario.ma@trantect.com>
2015-05-19 18:45:33 +0800

    fix nodebb 0.7

---
+++ README.md
@@ -0,0 +1,32 @@
+nodebb-plugin-emailer-local
+===========================
+
+NodeBB plugin for sending emails via SMTP services
+
+Installation
+---
+
+* Install this plugin via the "Plugins" page in the ACP
+* Then enable the plugin in NodeBB's Admin Control Panel in the Plugins tab.
+* Restart NodeBB
+
+Configuration
+---
+
+* Open NodeBB's Admin Control Panel
+* Click on the Emailer (SMTP) item in the Plugins section
+* Set hostname, port, user name and password fields
+* Click on Settings and set the desired address to use on the Email tab.
+
+Features to add
+---
+
+DKIM signatures  
+Popular services (Gmail, Hotmail, etc.)
+
+Credits
+---
+
+**Original Author**: [AlfredDobradi](https://github.com/AlfredDobradi)
+
+The NodeBB team is currently taking care of maintenance upgrades for this plugin (compatibility between versions). If anybody wishes to become the core maintainer of this plugin and add new features, please give us a shout on this plugin's [thread](https://community.nodebb.org/topic/496/-nodebb-plugin-emailer-local-local-email-plugin) on our community. :)
---
+++ index.js
@@ -0,0 +1,68 @@
+var fs = require('fs'),
+    path = require('path'),
+
+    winston = module.parent.require('winston'),
+    Meta = module.parent.require('./meta'),
+
+    nodemailer = require('nodemailer'),
+    Emailer = {};
+
+
+Emailer.init = function(data, callback) {
+    function renderAdminPage(req, res, next) {
+        res.render('admin/emailers/local', {});
+    }
+
+    data.router.get('/admin/emailers/local', data.middleware.admin.buildHeader, renderAdminPage);
+    data.router.get('/api/admin/emailers/local', renderAdminPage);
+
+    callback();
+};
+
+Emailer.send = function(data) {
+	Meta.settings.get('emailer-local', function(err, options){
+		var username = options['emailer:local:username'];
+		var pass = options['emailer:local:password'];
+		var transportOptions = {
+			host: options['emailer:local:host'],
+			port: options['emailer:local:port']
+		};
+		if( username || pass ) {
+			transportOptions.auth = {
+				user: username,
+				pass: pass
+			};
+		}
+		var transport = nodemailer.createTransport('SMTP', transportOptions);
+		transport.sendMail({
+			from: data.from,
+			to: data.to,
+			html: data.html,
+			text: data.plaintext,
+			subject: data.subject
+		},function(err,response) {
+			console.log(err);
+			if ( !err ) {
+				winston.info('[emailer.smtp] Sent `' + data.template + '` email to uid ' + data.uid);
+			} else {
+				winston.warn('[emailer.smtp] Unable to send `' + data.template + '` email to uid ' + data.uid + '!!');
+				// winston.error('[emailer.smtp] ' + response.message);
+			}
+		});
+	});
+
+}
+
+Emailer.admin = {
+    menu: function(custom_header, callback) {
+        custom_header.plugins.push({
+            "route": '/emailers/local',
+            "icon": 'fa-envelope-o',
+            "name": 'Emailer (Local)'
+        });
+
+        callback(null, custom_header);
+    }
+};
+
+module.exports = Emailer;
